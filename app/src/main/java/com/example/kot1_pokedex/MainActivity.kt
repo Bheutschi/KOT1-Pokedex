d848dcedb46146f586e04faf34573056ccb5d21f
bheutschi <bheutschi@gmail.com>
2026-06-08 11:15:45 +0200

    feat: set up RecyclerView with PokemonAdapter and GridLayout in MainActivity

--- app/src/main/java/com/example/kot1_pokedex/MainActivity.kt
+++ app/src/main/java/com/example/kot1_pokedex/MainActivity.kt
@@ -1,25 +1,39 @@
 package com.example.kot1_pokedex
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kot1_pokedex.data.RetrofitClient
 import com.example.kot1_pokedex.databinding.ActivityMainBinding
+import com.example.kot1_pokedex.ui.PokemonAdapter
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val adapter = PokemonAdapter{ pokemon -> }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.pokemonRecyclerView.layoutManager = GridLayoutManager(this, 2)
+        binding.pokemonRecyclerView.adapter = adapter
 
         loadPokemon()
 
